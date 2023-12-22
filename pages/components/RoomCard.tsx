f01bcb606f19c70b9672a660b44271641ad4a004
Menuchim <msezi.usv@gmail.com>
2023-12-22 16:50:38 +0100

    new

--- pages/components/RoomCard.tsx
+++ pages/components/RoomCard.tsx
@@ -2,22 +2,31 @@
 import React from "react";
 
 interface RoomDetailsProps {
-    room_type?: string;
+    room_type: string;
     img_url: string;
-    adults?: string;
-    children?: string;
-    room_size?: number;
-    room_price?: number; // Added room_price property
+    adults: string;
+    children: string;
+    room_size: number;
+    room_price: number; // Added room_price property
 }
 
-const RoomCard = ({details}:{details:RoomDetailsProps}) => {
+const defaultDetails = {
+  room_type: 'Signature Room',
+  img_url: 'https://images.pexels.com/photos/279746/pexels-photo-279746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  adults: '3',
+  children: '2',
+  room_size: 400,
+  room_price: 150000, // Price above or equal to 100000
+}
+
+const RoomCard = ({details = defaultDetails}:{details:RoomDetailsProps}) => {
   return (
     <div className="w-full">
-      {/* <img
+      <img
         className="w-full aspect-[4/3] md:aspect-[5/3]"
         src={details.img_url}
         alt=""
-      /> */}
+      />
       <article className="flex justify-between items-center gap-3 mt-3">
         <div className="space-y-1">
           <h4 className="text-4xl italic ibarra">{details.room_type}</h4>
